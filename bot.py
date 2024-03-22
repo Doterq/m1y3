e2d9c1d4612bef84839999f7e5db303d82945f36
Алексей <lolik19917@gmail.com>
2024-03-23 00:16:24 +1000

    Homework

--- bot.py
+++ bot.py
@@ -60,6 +60,10 @@ def make_some(message):
 def echo_message(message):
     bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 @bot.message_handler(content_types=['photo', 'file', 'voice'])
 def memr_photo(message):
